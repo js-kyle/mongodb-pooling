b905e5e97cafea3f12da68b4930b438e3e6bf1b7
Kyle Martin <kyle.martin@hotmail.co.nz>
2019-12-15 21:17:16 +1300

    init Github

---
+++ index.js
@@ -0,0 +1,36 @@
+const mongodb = require('mongodb');
+const express = require('express');
+
+const app = express();
+
+let db;
+const connectionOptions = { poolSize: process.env.MONGO_POOLSIZE || 1 };
+
+mongodb.MongoClient.connect('mongodb://localhost:27017/test', connectionOptions, function(err, database) {
+  if (err) throw err;
+  db = database.db('test');
+  // create some documents required for demonstration
+  db.collection('test').insertOne();
+
+  app.listen(process.env.PORT || 3000, function() {
+    console.log(`Express.js server up.`);
+  });
+});
+/*
+  For demonstration purposes, this route will always return slowly due to an intentionally slow query (5seconds per document)
+*/
+app.get('/slow', function (req, res) {
+  db.collection('test').find({'$where': 'sleep(5000) || true'}).toArray(function(err, cursor) {
+    return res.json({"docCount": 'docs'});
+  });
+	
+});
+/*
+  This route should always return quickly
+*/
+app.get('/fast', function (req, res) {
+  db.collection('test').countDoucments({}, function(err, count) {
+    return res.json({'documentCount': count});
+  });
+	
+});
